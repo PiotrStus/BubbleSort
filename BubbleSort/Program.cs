f432f4737ea965aebb64eb8ac3df6dd9841f60a4
Piotr Stus <pm2024love@gmail.com>
2024-03-30 12:02:30 +0100

    Stage 1 (pushing the highest number to the end)

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -2,9 +2,57 @@
 {
     internal class Program
     {
+        static List<int> numbersTwoSort = new List<int>();
+        static int firstNumber;
+        static int newNumber;
         static void Main(string[] args)
         {
-            Console.WriteLine("Bubble sort");
+            numbersTwoSort = [9, 6, 2, 4, 2, 1, 8, 1, 3];
+            Console.Write("Old Bubble sort: ");
+            returnList();
+
+
+            for (int i = 0; i < numbersTwoSort.Count; i++)
+            {
+                Console.WriteLine("Numbers to compare: ");
+                if ((i + 1) < numbersTwoSort.Count)
+                {
+                    Console.WriteLine(numbersTwoSort[i]);
+                    Console.WriteLine(numbersTwoSort[i + 1]);
+                }
+
+
+
+                if (((i + 1) < numbersTwoSort.Count) && numbersTwoSort[i] > numbersTwoSort[i+1] )
+                {
+                    Console.WriteLine("true");
+                    firstNumber = numbersTwoSort[i];
+                    newNumber = numbersTwoSort[i + 1];
+                    numbersTwoSort[i + 1] = firstNumber;
+                    numbersTwoSort[i] = newNumber;
+                    
+                }
+                else { 
+                Console.WriteLine("false");
+                }
+                returnList();
+            }
+            Console.WriteLine();
+            Console.Write("New Bubble sort: ");
+            returnList();
+            
+
+        }
+
+
+        static void returnList()
+        {
+            for (int i = 0; i < numbersTwoSort.Count; i++)
+            {
+                Console.Write(numbersTwoSort[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
